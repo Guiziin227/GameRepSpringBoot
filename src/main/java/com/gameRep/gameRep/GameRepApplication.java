57a97021eb313b44d938076c1da983dfa861599e
Guiziin227 <gh63388@gmail.com>
2025-05-19 13:50:20 -0300

    first commit

---
+++ src/main/java/com/gameRep/gameRep/GameRepApplication.java
@@ -0,0 +1,13 @@
+package com.gameRep.gameRep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameRepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameRepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gameRep/gameRep/GameRepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gameRep.gameRep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameRepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
