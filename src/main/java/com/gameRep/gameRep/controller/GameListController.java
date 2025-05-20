f5c8f467e25692ec2df32dd208031cb962deb064
Guiziin227 <gh63388@gmail.com>
2025-05-20 14:11:58 -0300

    feat: add GameListController for handling game list requests

---
+++ src/main/java/com/gameRep/gameRep/controller/GameListController.java
@@ -0,0 +1,25 @@
+package com.gameRep.gameRep.controller;
+
+import com.gameRep.gameRep.dto.GameListDto;
+import com.gameRep.gameRep.service.GameListService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(value = "/gameLists")
+public class GameListController {
+
+    private final GameListService gameListService;
+
+    @GetMapping
+    public List<GameListDto> findAll() {
+        List<GameListDto> result = gameListService.getAllLists();
+        return result;
+    }
+
+}
