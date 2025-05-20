4ed66eefeda6b5176aa51fa9fdc42d8eda78432e
Guiziin227 <gh63388@gmail.com>
2025-05-20 14:30:44 -0300

    feat: add findByList method to GameListController for retrieving games by list ID

--- src/main/java/com/gameRep/gameRep/controller/GameListController.java
+++ src/main/java/com/gameRep/gameRep/controller/GameListController.java
@@ -1,9 +1,12 @@
 package com.gameRep.gameRep.controller;
 
 import com.gameRep.gameRep.dto.GameListDto;
+import com.gameRep.gameRep.dto.GameMinDto;
 import com.gameRep.gameRep.service.GameListService;
+import com.gameRep.gameRep.service.GameService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,11 +18,15 @@ import java.util.List;
 public class GameListController {
 
     private final GameListService gameListService;
+    private final GameService gameService;
 
     @GetMapping
     public List<GameListDto> findAll() {
-        List<GameListDto> result = gameListService.getAllLists();
-        return result;
+        return gameListService.getAllLists();
     }
 
+    @GetMapping(value = "/{listId}/games")
+    public List<GameMinDto> findByList(@PathVariable Long listId) {
+        return gameService.findByList(listId);
+    }
 }
