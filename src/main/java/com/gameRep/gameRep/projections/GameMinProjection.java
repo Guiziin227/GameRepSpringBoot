c4146b5fa393befc891a744182de3d440a262cd8
Guiziin227 <gh63388@gmail.com>
2025-05-20 14:24:12 -0300

    feat: add searchByList method to GameRepository for retrieving games by list ID

---
+++ src/main/java/com/gameRep/gameRep/projections/GameMinProjection.java
@@ -0,0 +1,11 @@
+package com.gameRep.gameRep.projections;
+
+public interface GameMinProjection {
+    Long getId();
+    String getTitle();
+    Integer getYear();
+    String getImgUrl();
+    String getShortDescription();
+    Integer getPosition();
+
+}
